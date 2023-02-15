27be64622d9bea585872822094f8ee079ab5f7b1
Swappnet <advance11ua@gmail.com>
2023-02-15 13:05:28 +0200

    Adeed Scroll to top on path changing

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ import { useState } from 'react';
 import { useAppSelector } from './hooks/redux-hooks';
 import { BrowserRouter } from 'react-router-dom';
 import { Route, Routes } from 'react-router';
+import ScrollToTop from './hooks/ScrollToTop';
 
 function App() {
   const [isNewRoute, setIsNewRoute] = useState<boolean>(false);
@@ -14,6 +15,7 @@ function App() {
   return (
     <div className={`App theme-${darkMode}`}>
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route index element={<Start />}></Route>
           <Route path="/Editor" element={<Home />}></Route>
