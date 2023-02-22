660b8b75c4ef3995f5d9f18222ab777f62a9e0dd
Swappnet <advance11ua@gmail.com>
2023-02-22 10:38:34 +0200

    Changed picker btn style, changed defails section look

--- src/components/Info/Info.tsx
+++ src/components/Info/Info.tsx
@@ -8,18 +8,20 @@ export default function Info() {
       <h2 className="content-section--title">Details</h2>
       <div className="content-info--wrapper">
         <div className="content-info--box" title="Journey time">
-          <h4 className="content-info--box--value">{drawInfo.time}</h4>
-          <h4 className="content-info--box--title">
-            TIME
-            <span className="box--title--span">h</span>
-          </h4>
-        </div>
-        <div className="content-info--box" title="Journey distance">
-          <h4 className="content-info--box--value">{drawInfo.dist}</h4>
-          <h4 className="content-info--box--title">
-            DIST
-            <span className="box--title--span">km</span>
-          </h4>
+          <div className="info-content--wrapper">
+            <h4 className="content-info--box--value">{drawInfo.time}</h4>
+            <h4 className="content-info--box--title">
+              TIME
+              <span className="box--title--span">h</span>
+            </h4>
+          </div>
+          <div className="info-content--wrapper">
+            <h4 className="content-info--box--value">{drawInfo.dist}</h4>
+            <h4 className="content-info--box--title">
+              DIST
+              <span className="box--title--span">km</span>
+            </h4>
+          </div>
         </div>
       </div>
     </>
