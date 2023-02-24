68dfa05c0efa85b7815aab42b9075deb65f93713
Swappnet <advance11ua@gmail.com>
2023-02-24 18:36:27 +0200

    init

--- src/components/MapControls/MapControls.tsx
+++ src/components/MapControls/MapControls.tsx
@@ -68,6 +68,7 @@ export default function MapControls() {
   // Fit route on the map
   const handleRouteFit = () => {
     dispatch(changeFitBounds(true));
+    dispatch(changeLocationStatus(true));
   };
 
   // Get position of navigator geolocation
